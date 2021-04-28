fe60fb711841cba6b03bf52ec16231f40a24a6cb
Antoine <antoine.auger@gmail.com>
2021-04-28 17:46:23 +0100

    Milestone 2

--- app/src/main/java/com/example/minesweeper/MineView.java
+++ app/src/main/java/com/example/minesweeper/MineView.java
@@ -52,5 +52,50 @@ public class MineView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+
+        //Bounds of squares to be drawn.
+        int rectBounds = contentWidth / 10;
+
+        //Side length of the square.
+        int sideLength = rectBounds - 10;
+        //Paint instance for drawing the squares.
+        rectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        rectPaint.setColor(Color.BLACK);
+        touchedPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        touchedPaint.setColor(Color.GRAY);
+        bombPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        bombPaint.setColor(Color.RED);
+        flagPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        flagPaint.setColor(Color.YELLOW);
+        //Create a rect which is actually a square.
+        square = new Rect(10, 10, sideLength, sideLength);
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+
+                //Preliminary save of the drawing origin to the stack.
+                canvas.save();
+
+                //Translate to draw a row of squares. First will be at (0,0).
+                canvas.translate(i * rectBounds, j * rectBounds);
+                //Draw it.
+                canvas.drawRect(square, rectPaint);
+
+                //Restore. Back to the origin.
+                canvas.restore();
+
+            }// for j
+        }//for i
+
     }
 }
