8b64bbaf5419871fdcdeade1fd923a8b7a476766
Antoine <antoine.auger@gmail.com>
2021-04-28 18:01:15 +0100

    Milestone 3

--- app/src/main/java/com/example/minesweeper/MineView.java
+++ app/src/main/java/com/example/minesweeper/MineView.java
@@ -7,16 +7,44 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MineView extends View {
+    public static Button btn_reset;
+    public static Button flag;
+    public static TextView flags;
+
+    public static boolean isLose = false;
+    public static boolean isWin =false;
+    public static String stringFlag ="Flag : 0";
+
+    public static int flagPut;
+    public static int remainingMines;
     private Paint rectPaint;
     private Paint touchedPaint;
     private Paint bombPaint;
     private Paint flagPaint;
     private TextPaint Mine;
+
+    public static boolean start = true;
+    public static boolean flagon ;
+    private static boolean[][] isbomb = new boolean[10][10];
+    private static boolean[][] isCovered = new boolean[10][10];
+    private static int[][] neighbors = new int[10][10];
+    private static boolean[][] isFlag = new boolean[10][10];
+
     Rect square;
     int sideLength;
+
+    private int xtouch;
+    private int ytouch;
+    public static boolean touched =false;
 
 
 
@@ -47,6 +75,39 @@ public class MineView extends View {
     private void invalidateTextPaintAndMeasurements() {
 
     }//invalidateTextPaintAndMeasurements()
+
+    private OnTouchListener handleTouch= new OnTouchListener() {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            int paddingLeft = getPaddingLeft();
+            int paddingRight = getPaddingRight();
+            int contentWidth = getWidth() - paddingLeft - paddingRight;
+
+            //Bounds of squares to be drawn.
+            int rectBounds = contentWidth / 10;
+
+            if(event.getAction() == MotionEvent.ACTION_DOWN ){
+                xtouch = x / rectBounds;
+                ytouch = y / rectBounds;
+
+                if(ytouch<10 && xtouch<10 ) {
+                        isCovered[xtouch][ytouch] = false;
+                }
+                touched = true;
+                Log.i("TAG", "touched down on " + x / rectBounds + ", " + y / rectBounds);
+
+
+                invalidate();
+            }
+
+
+            return true;
+        }
+    };
+
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -80,6 +141,23 @@ public class MineView extends View {
         //Create a rect which is actually a square.
         square = new Rect(10, 10, sideLength, sideLength);
 
+
+        if(start){
+            for(int i=0; i<10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    isCovered[i][j] = true;
+                    isFlag[i][j] = false;
+                    isbomb[i][j] = false;
+                    neighbors[i][j]=0;
+                    canvas.save();
+                    canvas.translate(i * rectBounds, j * rectBounds);
+                    canvas.drawRect(square, rectPaint);
+                    canvas.restore();
+                }
+            }
+
+            start = false;
+        }
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
 
@@ -96,6 +174,39 @@ public class MineView extends View {
 
             }// for j
         }//for i
+
+        if(touched) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+
+                    if (isCovered[i][j] == true) {
+                        //Preliminary save of the drawing origin to the stack.
+                        canvas.save();
+
+                        //Translate to draw a row of squares. First will be at (0,0).
+                        canvas.translate(i * rectBounds, j * rectBounds);
+                        //Draw it.
+                        canvas.drawRect(square, rectPaint);
+
+                        //Restore. Back to the origin.
+                        canvas.restore();
+                    }
+                    if (isCovered[i][j] == false) {
+
+                        //Preliminary save of the drawing origin to the stack.
+                        canvas.save();
+                        //Translate to draw a row of squares. First will be at (0,0).
+                        canvas.translate(i * rectBounds, j * rectBounds);
+                        //Draw it.
+                        canvas.drawRect(square, touchedPaint);
+                        //Restore. Back to the origin.
+                        canvas.restore();
+                    }
+                }
+            }
+            touched=false;
+        }
+        setOnTouchListener(handleTouch);
 
     }
 }
